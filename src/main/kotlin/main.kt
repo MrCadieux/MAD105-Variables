485152dcf3b73745546f582047dc580c946cf739
Jeff Cadieux <jcadieux@d155.org>
2021-05-23 11:35:56 -0500

    Practice with variable declaration and use

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    var myInt: Int = 10
+    var myDouble: Double = 2.39
+    var myFloat: Float = 3.141592F
+    var myLong: Long = 20390693827869L
+    var myBoolean: Boolean = true
+    var myByte: Byte = 6
+    var myChar: Char = 'a'
+
+    println("Int demo $myInt")
+    println("Double demo $myDouble")
+    println("Float demo $myFloat")
+    println("Long demo $myLong")
+    println("Byte demo $myByte")
+    println("Char demo $myChar")
+    println("Boolean demo $myBoolean")
+
+
+}
